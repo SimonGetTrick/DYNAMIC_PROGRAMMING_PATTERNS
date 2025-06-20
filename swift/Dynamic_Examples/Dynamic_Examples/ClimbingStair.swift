6f9c951b7d3564166ccf38ac746563bc493c0a9a
Simon Getrik <SimonGetTrick@users.noreply.github.com>
2025-06-20 09:04:22 +0200

    Robber houses

--- swift/Dynamic_Examples/Dynamic_Examples/ClimbingStair.swift
+++ swift/Dynamic_Examples/Dynamic_Examples/ClimbingStair.swift
@@ -7,6 +7,36 @@
 
 import Foundation
 class Solution {
+    // Maximum robber dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+    func rob(_ nums: [Int]) -> Int {
+        let n = nums.count
+        if n == 0 { return 0 }
+        if n == 1 { return nums[0] }
+        
+        var dp = Array(repeating: 0, count: n)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        
+        for i in 2..<n {
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+        }
+        
+        return dp[n - 1]
+    }
+    // Time complexity: O(n) Space complexity: O(1)
+    func robMin(_ nums: [Int]) -> Int {
+          var prev1 = 0
+          var prev2 = 0
+          
+          for num in nums {
+              let temp = max(prev1, prev2 + num)
+              prev2 = prev1
+              prev1 = temp
+          }
+          
+          return prev1
+      }
+    
     // iterative time O(n), memory — O(1).
     func climbStairs(_ n: Int) -> Int {
         if n <= 2 {
@@ -49,8 +79,27 @@ class Solution {
     }
 }
 /*
- task:
- Climbing Stairs
+ task: Rober
+ ou are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+ Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+  
+
+ Example 1:
+
+ Input: nums = [1,2,3,1]
+ Output: 4
+ Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+ Total amount you can rob = 1 + 3 = 4.
+ Example 2:
+
+ Input: nums = [2,7,9,3,1]
+ Output: 12
+ Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+ Total amount you can rob = 2 + 9 + 1 = 12.
+  
+ task: Climbing Stairs
  Easy
  Topics
  premium lock icon
