3dc6918a564fdc04115550518d21f9d721da8c78
Simon Getrik <SimonGetTrick@users.noreply.github.com>
2025-07-11 10:36:03 +0200

    Container with Most Water

--- swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
+++ swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
@@ -8,6 +8,60 @@
 import Foundation
 class Solution {
     /*
+     You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+     Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+     Return the maximum amount of water a container can store.
+
+     Notice that you may not slant the container.
+
+      
+
+     Example 1:
+     barArea.png
+
+     Input: height = [1,8,6,2,5,4,8,3,7]
+     Output: 49
+     Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+     Example 2:
+
+     Input: height = [1,1]
+     Output: 1
+      
+
+     Constraints:
+
+     n == height.length
+     2 <= n <= 105
+     0 <= height[i] <= 104
+     */
+    func maxArea(_ height: [Int]) -> Int {
+        var left = 0
+        var right = height.count - 1
+        var maxWater = 0
+
+        while left < right {
+            // Calculate width
+            let width = right - left
+            // Minimum of two heights defines container height
+            let minHeight = min(height[left], height[right])
+            // Compute current area
+            let currentWater = width * minHeight
+            maxWater = max(maxWater, currentWater)
+
+            // Move pointer that has the smaller height
+            if height[left] < height[right] {
+                left += 1
+            } else {
+                right -= 1
+            }
+        }
+
+        return maxWater
+    }
+
+    /*
      Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
 
      '.' Matches any single character.
