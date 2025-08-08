add6fd4886cb6b7f1f0bddc947b2a1f6d07bda27
Simon Getrik <SimonGetTrick@users.noreply.github.com>
2025-08-08 10:26:57 +0200

    Rotate Image

--- swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
+++ swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
@@ -7,6 +7,60 @@
 
 import Foundation
 class Solution {
+    /*
+     48. Rotate Image
+     Medium
+     Topics
+     premium lock icon
+     Companies
+     You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+     You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+     Example 1:
+     Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+     Output: [[7,4,1],[8,5,2],[9,6,3]]
+     Example 2:
+     Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+     Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+     Constraints:
+     n == matrix.length == matrix[i].length
+     1 <= n <= 20
+     -1000 <= matrix[i][j] <= 1000
+     */
+    class RotateImage {
+        static func runDemo() {
+            var m1 = [[1,2,3],
+                      [4,5,6],
+                      [7,8,9]]
+            rotate(&m1)
+            print(m1) // [[7,4,1],[8,5,2],[9,6,3]]
+
+            var m2 = [[5,1,9,11],
+                      [2,4,8,10],
+                      [13,3,6,7],
+                      [15,14,12,16]]
+            rotate(&m2)
+            print(m2) // [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+        }
+
+        static func rotate(_ matrix: inout [[Int]]) {
+            let n = matrix.count
+            
+            // Step 1: Transpose (swap matrix[i][j] with matrix[j][i])
+            for i in 0..<n {
+                for j in i..<n {
+                    let temp = matrix[i][j]
+                    matrix[i][j] = matrix[j][i]
+                    matrix[j][i] = temp
+                }
+            }
+
+            // Step 2: Reverse each row
+            for i in 0..<n {
+                matrix[i].reverse()
+            }
+        }
+    }
+
     /*46. Permutations
      Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
      Example 1:
