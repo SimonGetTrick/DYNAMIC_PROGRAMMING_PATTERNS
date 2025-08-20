6abfd19f23fc7183e1dce71c9372da3a7605df58
Simon Getrik <SimonGetTrick@users.noreply.github.com>
2025-08-20 16:11:19 +0200

    62. Unique Paths

--- swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
+++ swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
@@ -8,6 +8,41 @@
 import Foundation
 class Solution {
     /*
+     62. Unique Paths
+     Medium
+     Topics
+     premium lock icon
+     Companies
+     There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+     Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+     The test cases are generated so that the answer will be less than or equal to 2 * 109.
+     Example 1:
+     Input: m = 3, n = 7
+     Output: 28
+     Example 2:
+     Input: m = 3, n = 2
+     Output: 3
+     Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+     1. Right -> Down -> Down
+     2. Down -> Down -> Right
+     3. Down -> Right -> Down
+     Constraints:
+     1 <= m, n <= 100
+     */
+    func uniquePaths(_ m: Int, _ n: Int) -> Int {
+        // Initialize a 2D dp array filled with 1's
+        var dp = Array(repeating: Array(repeating: 1, count: n), count: m)
+        
+        // Fill the dp table
+        for i in 1..<m {
+            for j in 1..<n {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+            }
+        }
+        
+        return dp[m-1][n-1]
+    }
+    /*
      61. Rotate List
      Given the head of a linked list, rotate the list to the right by k places.
      Example 1:
