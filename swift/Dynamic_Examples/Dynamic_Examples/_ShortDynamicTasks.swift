8fd8f63be29de662df85ef865c2c56e4edcc651f
Simon Getrik <SimonGetTrick@users.noreply.github.com>
2025-08-21 08:09:44 +0200

    63. Unique Paths II

--- swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
+++ swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
@@ -8,6 +8,67 @@
 import Foundation
 class Solution {
     /*
+     63. Unique Paths II
+     Medium
+     Topics
+     premium lock icon
+     Companies
+     Hint
+     You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+     An obstacle and space are marked as 1 or 0 respectively in grid. A path that the robot takes cannot include any square that is an obstacle.
+     Return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+     The testcases are generated so that the answer will be less than or equal to 2 * 109.
+     Example 1:
+     Input: obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+     Output: 2
+     Explanation: There is one obstacle in the middle of the 3x3 grid above.
+     There are two ways to reach the bottom-right corner:
+     1. Right -> Right -> Down -> Down
+     2. Down -> Down -> Right -> Right
+     Example 2:
+     Input: obstacleGrid = [[0,1],[0,0]]
+     Output: 1
+     Constraints:
+     m == obstacleGrid.length
+     n == obstacleGrid[i].length
+     1 <= m, n <= 100
+     obstacleGrid[i][j] is 0 or 1.
+     */
+    func uniquePathsWithObstacles(_ obstacleGrid: [[Int]]) -> Int {
+        let m = obstacleGrid.count
+        let n = obstacleGrid[0].count
+        
+        // If start or finish is blocked, no paths exist
+        if obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1 {
+            return 0
+        }
+        
+        // Use 1D DP array for memory optimization
+        var dp = Array(repeating: 0, count: n)
+        dp[0] = 1
+        
+        for i in 0..<m {
+            for j in 0..<n {
+                // If current cell is an obstacle → zero paths
+                if obstacleGrid[i][j] == 1 {
+                    dp[j] = 0
+                } else if j > 0 {
+                    // Add paths from the left cell
+                    dp[j] += dp[j - 1]
+                }
+            }
+        }
+        
+        return dp[n - 1]
+    }
+    func demo_uniquePathsWithObstacles() {
+        let sol = self //Solution()
+        print(sol.uniquePathsWithObstacles([[0,0,0],[0,1,0],[0,0,0]])) // 2
+        print(sol.uniquePathsWithObstacles([[0,1],[0,0]]))             // 1
+        print(sol.uniquePathsWithObstacles([[1,0]]))                   // 0
+        print(sol.uniquePathsWithObstacles([[0,0],[1,1],[0,0]]))       // 0
+    }
+    /*
      62. Unique Paths
      Medium
      Topics
