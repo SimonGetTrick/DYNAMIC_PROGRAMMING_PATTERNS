bb385899c8445ce945dd85bb7aac996a80f2d8c4
Simon Getrik <SimonGetTrick@users.noreply.github.com>
2025-08-28 07:38:39 +0200

    75. Sort Colors

--- swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
+++ swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
@@ -8,6 +8,30 @@
 import Foundation
 class Solution {
     /*
+     75. Sort Colors Medium Topics premium lock icon Companies Hint Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue. We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively. You must solve this problem without using the library's sort function. Example 1: Input: nums = [2,0,2,1,1,0] Output: [0,0,1,1,2,2] Example 2: Input: nums = [2,0,1] Output: [0,1,2] Constraints: n == nums.length 1 <= n <= 300 nums[i] is either 0, 1, or 2. Follow up: Could you come up with a one-pass algorithm using only constant extra space?
+     */
+    func sortColors(_ nums: inout [Int]) {
+        var low = 0          // Next position for 0
+        var mid = 0          // Current index
+        var high = nums.count - 1 // Next position for 2
+        
+        while mid <= high {
+            if nums[mid] == 0 {
+                // Swap 0 to the front
+                nums.swapAt(low, mid)
+                low += 1
+                mid += 1
+            } else if nums[mid] == 1 {
+                // 1 is in the correct place
+                mid += 1
+            } else {
+                // Swap 2 to the end
+                nums.swapAt(mid, high)
+                high -= 1
+            }
+        }
+    }
+    /*
      74. Search a 2D Matrix
      Companies You are given an m x n integer matrix matrix with the following two properties: Each row is sorted in non-decreasing order. The first integer of each row is greater than the last integer of the previous row. Given an integer target, return true if target is in matrix or false otherwise. You must write a solution in O(log(m * n)) time complexity. Example 1: Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3 Output: true Example 2: Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13 Output: false Constraints: m == matrix.length n == matrix[i].length 1 <= m, n <= 100 -104 <= matrix[i][j], target <= 104
      */
