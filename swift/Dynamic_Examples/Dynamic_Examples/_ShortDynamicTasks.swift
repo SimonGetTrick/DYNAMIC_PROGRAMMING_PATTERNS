51b59fe94deaa0e069c0893ba52d8e64f3bec73a
Simon Getrik <SimonGetTrick@users.noreply.github.com>
2025-08-31 11:33:10 +0200

    78. Subsets

--- swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
+++ swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
@@ -8,6 +8,33 @@
 import Foundation
 class Solution {
     /*
+     78. Subsets Medium Topics premium lock icon Companies Given an integer array nums of unique elements, return all possible subsets (the power set). The solution set must not contain duplicate subsets. Return the solution in any order. Example 1: Input: nums = [1,2,3] Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]] Example 2: Input: nums = [0] Output: [[],[0]] Constraints: 1 <= nums.length <= 10 -10 <= nums[i] <= 10 All the numbers of nums are unique.
+     */
+    func subsets(_ nums: [Int]) -> [[Int]] {
+        var result = [[Int]]()      // Final list of subsets
+        var subset = [Int]()        // Temporary subset
+        
+        func backtrack(_ index: Int) {
+            // Add current subset to result
+            result.append(subset)
+            
+            // Explore further elements
+            for i in index..<nums.count {
+                // Include nums[i] in subset
+                subset.append(nums[i])
+                
+                // Recurse with next index
+                backtrack(i + 1)
+                
+                // Backtrack: remove last element
+                subset.removeLast()
+            }
+        }
+        
+        backtrack(0) // Start from index 0
+        return result
+    }
+    /*
      77. Combinations Medium Topics premium lock icon Companies Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n]. You may return the answer in any order. Example 1: Input: n = 4, k = 2 Output: [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]] Explanation: There are 4 choose 2 = 6 total combinations. Note that combinations are unordered, i.e., [1,2] and [2,1] are considered to be the same combination. Example 2: Input: n = 1, k = 1 Output: [[1]] Explanation: There is 1 choose 1 = 1 total combination.
      */
     func combine(_ n: Int, _ k: Int) -> [[Int]] {
