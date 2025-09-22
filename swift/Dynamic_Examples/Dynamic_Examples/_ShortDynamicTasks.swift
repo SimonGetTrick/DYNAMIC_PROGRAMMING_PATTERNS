765c214bed03e5472a5656afeb0d5df7dfecfe0f
Simon Getrik <SimonGetTrick@users.noreply.github.com>
2025-09-22 18:08:08 +0200

    123. Best Time to Buy and Sell Stock III

--- swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
+++ swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
@@ -8,6 +8,55 @@
 import Foundation
 class Solution {
     /*
+     123. Best Time to Buy and Sell Stock III
+     You are given an array prices where prices[i] is the price of a given stock on the ith day.
+     Find the maximum profit you can achieve. You may complete at most two transactions.
+     Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+     Example 1:
+     Input: prices = [3,3,5,0,0,3,1,4]
+     Output: 6
+     Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+     Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+     Example 2:
+     Input: prices = [1,2,3,4,5]
+     Output: 4
+     Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+     Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are engaging multiple transactions at the same time. You must sell before buying again.
+     Example 3:
+     Input: prices = [7,6,4,3,1]
+     Output: 0
+     Explanation: In this case, no transaction is done, i.e. max profit = 0.
+     */
+    class StockProfit {
+        // 123. Best Time to Buy and Sell Stock III
+        func maxProfit(_ prices: [Int]) -> Int {
+            if prices.isEmpty { return 0 }
+            
+            // Initialize states
+            var buy1 = Int.min
+            var sell1 = 0
+            var buy2 = Int.min
+            var sell2 = 0
+            
+            for price in prices {
+                // Max profit after first buy (spend money)
+                buy1 = max(buy1, -price)
+                
+                // Max profit after first sell (gain money)
+                sell1 = max(sell1, buy1 + price)
+                
+                // Max profit after second buy (spend money again)
+                buy2 = max(buy2, sell1 - price)
+                
+                // Max profit after second sell (final gain)
+                sell2 = max(sell2, buy2 + price)
+            }
+            
+            return sell2
+        }
+    }
+    
+    /*
      116. Populating Next Right Pointers in Each Node Medium Topics premium lock icon Companies You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition: struct Node { int val; Node *left; Node *right; Node *next; } Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL. Initially, all next pointers are set to NULL. Example 1: Input: root = [1,2,3,4,5,6,7] Output: [1,#,2,3,#,4,5,6,7,#] Explanation: Given the above perfect binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level. Example 2: Input: root = [] Output: []
      */
     public class Node {
