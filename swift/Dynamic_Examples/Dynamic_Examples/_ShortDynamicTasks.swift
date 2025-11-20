7844bc1607794d2a841a969ef0156a85260c08ae
Simon Getrik <SimonGetTrick@users.noreply.github.com>
2025-11-20 08:48:49 +0100

    208. Implement Trie (Prefix Tree)

--- swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
+++ swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
@@ -16,6 +16,73 @@ extension String {
 
 class Solution {
     /*
+     208. Implement Trie (Prefix Tree)
+     A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
+     Implement the Trie class:
+     Trie() Initializes the trie object.
+     void insert(String word) Inserts the string word into the trie.
+     boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+     boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+     Example 1:
+     Input
+     ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+     [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+     Output
+     [null, null, true, false, true, null, true]
+     Explanation
+     Trie trie = new Trie();
+     trie.insert("apple");
+     trie.search("apple");   // return True
+     trie.search("app");     // return False
+     trie.startsWith("app"); // return True
+     trie.insert("app");
+     trie.search("app");     // return True
+     Constraints:
+     1 <= word.length, prefix.length <= 2000
+     word and prefix consist only of lowercase English letters.
+     At most 3 * 104 calls in total will be made to insert, search, and startsWith.
+     */
+    class TrieNode {
+        var children: [Character: TrieNode] = [:]
+        var isWord = false
+    }
+
+    class Trie {
+
+        private let root = TrieNode()
+
+        init() {}
+
+        func insert(_ word: String) {
+            var node = root
+            for ch in word {
+                if node.children[ch] == nil {
+                    node.children[ch] = TrieNode()
+                }
+                node = node.children[ch]!
+            }
+            node.isWord = true
+        }
+
+        func search(_ word: String) -> Bool {
+            var node = root
+            for ch in word {
+                guard let next = node.children[ch] else { return false }
+                node = next
+            }
+            return node.isWord
+        }
+
+        func startsWith(_ prefix: String) -> Bool {
+            var node = root
+            for ch in prefix {
+                guard let next = node.children[ch] else { return false }
+                node = next
+            }
+            return true
+        }
+    }
+    /*
      207. Course Schedule
      There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
 
