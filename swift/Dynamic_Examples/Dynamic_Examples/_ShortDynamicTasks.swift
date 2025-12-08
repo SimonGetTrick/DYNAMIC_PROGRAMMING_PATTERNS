702416fcba7bf6dc3a48bfc59f86c6f19602eef2
Simon Getrik <SimonGetTrick@users.noreply.github.com>
2025-12-08 14:21:10 +0100

    227. Basic Calculator

--- swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
+++ swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
@@ -27,6 +27,62 @@ extension Array where Element == Int {
 
 class Solution {
     /*
+     227. Basic Calculator II
+     Given a string s which represents an expression, evaluate this expression and return its value.
+     The integer division should truncate toward zero.
+     You may assume that the given expression is always valid. All intermediate results will be in the range of [-231, 231 - 1].
+     Note: You are not allowed to use any built-in function which evaluates strings as mathematical expressions, such as eval().
+     Example 1:
+     Input: s = "3+2*2"
+     Output: 7
+     Example 2:
+     Input: s = " 3/2 "
+     Output: 1
+     Example 3:
+     Input: s = " 3+5 / 2 "
+     Output: 5
+     Constraints:
+     1 <= s.length <= 3 * 105
+     s consists of integers and operators ('+', '-', '*', '/') separated by some number of spaces.
+     s represents a valid expression.
+     All the integers in the expression are non-negative integers in the range [0, 231 - 1].
+     The answer is guaranteed to fit in a 32-bit integer.
+     */
+    func calculate227(_ s: String) -> Int {
+            var num = 0
+            var lastOp: Character = "+"
+            var stack = [Int]()
+            let chars = Array(s)
+            
+            for i in 0..<chars.count {
+                let ch = chars[i]
+                
+                if let d = ch.wholeNumberValue {
+                    num = num * 10 + d
+                }
+                
+                if (!ch.isNumber && ch != " ") || i == chars.count - 1 {
+                    switch lastOp {
+                    case "+":
+                        stack.append(num)
+                    case "-":
+                        stack.append(-num)
+                    case "*":
+                        stack.append(stack.removeLast() * num)
+                    case "/":
+                        stack.append(stack.removeLast() / num)
+                    default:
+                        break
+                    }
+                    
+                    lastOp = ch
+                    num = 0
+                }
+            }
+            
+            return stack.reduce(0, +)
+        }
+    /*
      226. Invert Binary Tree
      Given the root of a binary tree, invert the tree, and return its root.
      Example 1:
