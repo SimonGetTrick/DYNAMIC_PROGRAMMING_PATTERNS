b8a716721aab68e7425234e57bf44c2b2a3acf08
Simon Getrik <SimonGetTrick@users.noreply.github.com>
2025-12-09 13:12:16 +0100

    228. Summary Ranges

--- swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
+++ swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
@@ -27,6 +27,57 @@ extension Array where Element == Int {
 
 class Solution {
     /*
+     228. Summary Ranges
+     You are given a sorted unique integer array nums.
+     A range [a,b] is the set of all integers from a to b (inclusive).
+     Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+     Each range [a,b] in the list should be output as:
+     "a->b" if a != b
+     "a" if a == b
+     Example 1:
+     Input: nums = [0,1,2,4,5,7]
+     Output: ["0->2","4->5","7"]
+     Explanation: The ranges are:
+     [0,2] --> "0->2"
+     [4,5] --> "4->5"
+     [7,7] --> "7"
+     Example 2:
+     Input: nums = [0,2,3,4,6,8,9]
+     Output: ["0","2->4","6","8->9"]
+     Explanation: The ranges are:
+     [0,0] --> "0"
+     [2,4] --> "2->4"
+     [6,6] --> "6"
+     [8,9] --> "8->9"
+     */
+    func summaryRanges228(_ nums: [Int]) -> [String] {
+            guard !nums.isEmpty else { return [] }
+
+            var res = [String]()
+            var start = nums[0]
+
+            for i in 1..<nums.count {
+                // проверяем разрыв последовательности
+                if nums[i] != nums[i - 1] + 1 {
+                    if start == nums[i - 1] {
+                        res.append("\(start)")
+                    } else {
+                        res.append("\(start)->\(nums[i - 1])")
+                    }
+                    start = nums[i]
+                }
+            }
+
+            // записываем последний диапазон
+            if start == nums.last! {
+                res.append("\(start)")
+            } else {
+                res.append("\(start)->\(nums.last!)")
+            }
+
+            return res
+        }
+    /*
      227. Basic Calculator II
      Given a string s which represents an expression, evaluate this expression and return its value.
      The integer division should truncate toward zero.
