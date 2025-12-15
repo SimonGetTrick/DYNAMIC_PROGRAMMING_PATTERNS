85559f2a1e9dcf6d05825dfc0ead5331c983b04d
Simon Getrik <SimonGetTrick@users.noreply.github.com>
2025-12-15 13:41:19 +0100

    237. Delete Node in a Linked List

--- swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
+++ swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
@@ -27,6 +27,57 @@ extension Array where Element == Int {
 
 class Solution {
     /*
+     237. Delete Node in a Linked List
+     There is a singly-linked list head and we want to delete a node node in it.
+
+     You are given the node to be deleted node. You will not be given access to the first node of head.
+
+     All the values of the linked list are unique, and it is guaranteed that the given node node is not the last node in the linked list.
+
+     Delete the given node. Note that by deleting the node, we do not mean removing it from memory. We mean:
+
+     The value of the given node should not exist in the linked list.
+     The number of nodes in the linked list should decrease by one.
+     All the values before node should be in the same order.
+     All the values after node should be in the same order.
+     Custom testing:
+     For the input, you should provide the entire linked list head and the node to be given node. node should not be the last node of the list and should be an actual node in the list.
+     We will build the linked list and pass the node to your function.
+     The output will be the entire list after calling your function.
+     Example 1:
+     Input: head = [4,5,1,9], node = 5
+     Output: [4,1,9]
+     Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.
+     Example 2:
+     Input: head = [4,5,1,9], node = 1
+     Output: [4,5,9]
+     Explanation: You are given the third node with value 1, the linked list should become 4 -> 5 -> 9 after calling your function.
+     Constraints:
+     The number of the nodes in the given list is in the range [2, 1000].
+     -1000 <= Node.val <= 1000
+     The value of each node in the list is unique.
+     The node to be deleted is in the list and is not a tail node.
+     */
+    // 237. Delete Node in a Linked List
+    // Time Complexity: O(1)
+    // Space Complexity: O(1)
+
+    class LC237_DeleteNodeInLinkedList {
+
+        func deleteNode(_ node: ListNode?) {
+            guard let node = node, let next = node.next else {
+                return
+            }
+
+            // Copy the value from the next node
+            node.val = next.val
+
+            // Skip the next node
+            node.next = next.next
+        }
+    }
+
+    /*
      236. Lowest Common Ancestor of a Binary Tree
      Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
      According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
