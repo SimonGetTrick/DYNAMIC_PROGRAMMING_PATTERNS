ded09c792b006dd9052aac66eb55dbe0205ed74e
Simon Getrik <SimonGetTrick@users.noreply.github.com>
2026-01-14 03:49:22 +0100

    307. Range Sum Query - Mutable

--- swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
+++ swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
@@ -27,6 +27,93 @@ extension Array where Element == Int {
 
 class Solution {
     /*
+     307. Range Sum Query - Mutable
+     Given an integer array nums, handle multiple queries of the following types:
+     Update the value of an element in nums.
+     Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+     Implement the NumArray class:
+     NumArray(int[] nums) Initializes the object with the integer array nums.
+     void update(int index, int val) Updates the value of nums[index] to be val.
+     int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+
+     Example 1:
+     Input
+     ["NumArray", "sumRange", "update", "sumRange"]
+     [[[1, 3, 5]], [0, 2], [1, 2], [0, 2]]
+     Output
+     [null, 9, null, 8]
+     Explanation
+     NumArray numArray = new NumArray([1, 3, 5]);
+     numArray.sumRange(0, 2); // return 1 + 3 + 5 = 9
+     numArray.update(1, 2);   // nums = [1, 2, 5]
+     numArray.sumRange(0, 2); // return 1 + 2 + 5 = 8
+      
+
+     Constraints:
+
+     1 <= nums.length <= 3 * 104
+     -100 <= nums[i] <= 100
+     0 <= index < nums.length
+     -100 <= val <= 100
+     0 <= left <= right < nums.length
+     At most 3 * 104 calls will be made to update and sumRange.
+     */
+    class NumArray307 {
+        private var tree: [Int]
+        private var nums: [Int]
+        private let n: Int
+        
+        init(_ nums: [Int]) {
+            self.nums = nums
+            self.n = nums.count
+            self.tree = Array(repeating: 0, count: n + 1)
+            
+            // Build Fenwick Tree
+            for i in 0..<n {
+                add(i + 1, nums[i])
+            }
+        }
+        
+        // Update value at index
+        func update(_ index: Int, _ val: Int) {
+            let diff = val - nums[index]
+            nums[index] = val
+            add(index + 1, diff)
+        }
+        
+        // Return sum in range [left, right]
+        func sumRange(_ left: Int, _ right: Int) -> Int {
+            return prefixSum(right + 1) - prefixSum(left)
+        }
+        
+        // MARK: - Fenwick Tree helpers
+        
+        private func add(_ index: Int, _ value: Int) {
+            var i = index
+            while i <= n {
+                tree[i] += value
+                i += i & -i
+            }
+        }
+        
+        private func prefixSum(_ index: Int) -> Int {
+            var i = index
+            var sum = 0
+            while i > 0 {
+                sum += tree[i]
+                i -= i & -i
+            }
+            return sum
+        }
+    }
+
+    // Пример использования
+    let numArray = NumArray307([1, 3, 5])
+    print(numArray.sumRange(0, 2)) // 9
+    numArray.update(1, 2)
+    print(numArray.sumRange(0, 2)) // 8
+
+    /*
      306. Additive Number
      An additive number is a string whose digits can form an additive sequence.
      A valid additive sequence should contain at least three numbers. Except for the first two numbers, each subsequent number in the sequence must be the sum of the preceding two.
