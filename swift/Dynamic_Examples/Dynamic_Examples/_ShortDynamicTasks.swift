67d75daf1a8f3a7ea86a8bbbddd92cd2654863c0
Simon Getrik <SimonGetTrick@users.noreply.github.com>
2026-01-24 11:33:23 +0100

    324. Wiggle Sort II

--- swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
+++ swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
@@ -27,6 +27,56 @@ extension Array where Element == Int {
 
 class Solution {
     /*
+     324. Wiggle Sort II
+     Given an integer array nums, reorder it such that nums[0] < nums[1] > nums[2] < nums[3]....
+     You may assume the input array always has a valid answer.
+     Example 1:
+     Input: nums = [1,5,1,1,6,4]
+     Output: [1,6,1,5,1,4]
+     Explanation: [1,4,1,5,1,6] is also accepted.
+     Example 2:
+     Input: nums = [1,3,2,2,3,1]
+     Output: [2,3,1,3,1,2]
+     Constraints:
+     1 <= nums.length <= 5 * 104
+     0 <= nums[i] <= 5000
+     It is guaranteed that there will be an answer for the given input nums.
+     Follow Up: Can you do it in O(n) time and/or in-place with O(1) extra space?
+     */
+    class Solution324 {
+        // Static demo method
+        static func runDemo() {
+            var nums1 = [1,5,1,1,6,4]
+            wiggleSort(&nums1)
+            print(nums1)
+            
+            var nums2 = [1,3,2,2,3,1]
+            wiggleSort(&nums2)
+            print(nums2)
+        }
+        
+        // Reorders nums into wiggle sort order
+        private static func wiggleSort(_ nums: inout [Int]) {
+            let n = nums.count
+            let sorted = nums.sorted()
+            
+            var left = (n - 1) / 2   // End of first (smaller) half
+            var right = n - 1        // End of second (larger) half
+            
+            // Place elements alternately
+            for i in 0..<n {
+                if i % 2 == 0 {
+                    nums[i] = sorted[left]
+                    left -= 1
+                } else {
+                    nums[i] = sorted[right]
+                    right -= 1
+                }
+            }
+        }
+    }
+
+    /*
      322. Coin Change
      You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
      Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
