913047b1680e6bf2020fafedac081c0467c15c63
Simon Getrik <SimonGetTrick@users.noreply.github.com>
2026-02-15 11:33:09 +0100

    365. Water and Jug Problem

--- swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
+++ swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
@@ -27,6 +27,57 @@ extension Array where Element == Int {
 
 class Solution {
     /*
+     365. Water and Jug Problem
+     You are given two jugs with capacities x liters and y liters. You have an infinite water supply. Return whether the total amount of water in both jugs may reach target using the following operations:
+     Fill either jug completely with water.
+     Completely empty either jug.
+     Pour water from one jug into another until the receiving jug is full, or the transferring jug is empty.
+     Example 1:
+     Input: x = 3, y = 5, target = 4
+     Output: true
+     Explanation:
+     Follow these steps to reach a total of 4 liters:
+     Fill the 5-liter jug (0, 5).
+     Pour from the 5-liter jug into the 3-liter jug, leaving 2 liters (3, 2).
+     Empty the 3-liter jug (0, 2).
+     Transfer the 2 liters from the 5-liter jug to the 3-liter jug (2, 0).
+     Fill the 5-liter jug again (2, 5).
+     Pour from the 5-liter jug into the 3-liter jug until the 3-liter jug is full. This leaves 4 liters in the 5-liter jug (3, 4).
+     Empty the 3-liter jug. Now, you have exactly 4 liters in the 5-liter jug (0, 4).
+     Reference: The Die Hard example.
+     Example 2:
+     Input: x = 2, y = 6, target = 5
+     Output: false
+     Example 3:
+     Input: x = 1, y = 2, target = 3
+     Output: true
+     Explanation: Fill both jugs. The total amount of water in both jugs is equal to 3 now.
+     */
+    class Solution365 {
+        
+        static func canMeasureWater(_ x: Int, _ y: Int, _ target: Int) -> Bool {
+            if target == 0 { return true }
+            if x + y < target { return false }
+            
+            return target % gcd(x, y) == 0
+        }
+        
+        
+        private static func gcd(_ a: Int, _ b: Int) -> Int {
+            var a = a
+            var b = b
+            
+            while b != 0 {
+                let temp = a % b
+                a = b
+                b = temp
+            }
+            
+            return a
+        }
+    }
+
+    /*
      363. Max Sum of Rectangle No Larger Than K-- Hard
      Given an m x n matrix matrix and an integer k, return the max sum of a rectangle in the matrix such that its sum is no larger than k.
      It is guaranteed that there will be a rectangle with a sum no larger than k.
