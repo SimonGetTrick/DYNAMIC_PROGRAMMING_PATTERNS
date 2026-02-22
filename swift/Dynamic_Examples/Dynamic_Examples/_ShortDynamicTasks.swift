e426e5cb0a51de5ad899b304b9adcd0ed78d927d
Simon Getrik <SimonGetTrick@users.noreply.github.com>
2026-02-22 19:03:42 +0100

    377. Combination Sum IV

--- swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
+++ swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
@@ -27,6 +27,67 @@ extension Array where Element == Int {
 
 class Solution {
     /*
+     377. Combination Sum IV
+     Given an array of distinct integers nums and a target integer target, return the number of possible combinations that add up to target.
+     The test cases are generated so that the answer can fit in a 32-bit integer.
+     Example 1:
+     Input: nums = [1,2,3], target = 4
+     Output: 7
+     Explanation:
+     The possible combination ways are:
+     (1, 1, 1, 1)
+     (1, 1, 2)
+     (1, 2, 1)
+     (1, 3)
+     (2, 1, 1)
+     (2, 2)
+     (3, 1)
+     Note that different sequences are counted as different combinations.
+     Example 2:
+     Input: nums = [9], target = 3
+     Output: 0
+     Constraints:
+     1 <= nums.length <= 200
+     1 <= nums[i] <= 1000
+     All the elements of nums are unique.
+     1 <= target <= 1000
+     Follow up: What if negative numbers are allowed in the given array? How does it change the problem? What limitation we need to add to the question to allow negative numbers?
+     */
+    // 377. Combination Sum IV
+    // Dynamic Programming O(target * nums.count)
+
+    enum Leet377 {
+        
+        class CombinationSumIV {
+            
+            // Returns number of combinations that sum to target
+            static func combinationSum4(_ nums: [Int], _ target: Int) -> Int {
+                if target < 0 { return 0 }
+                
+                var dp = Array(repeating: 0, count: target + 1)
+                dp[0] = 1
+                
+                for t in 1...target {
+                    for num in nums {
+                        if t - num >= 0 {
+                            dp[t] += dp[t - num]
+                        }
+                    }
+                }
+                
+                return dp[target]
+            }
+            
+            static func runDemo() {
+                let nums1 = [1,2,3]
+                print(combinationSum4(nums1, 4)) // 7
+                
+                let nums2 = [9]
+                print(combinationSum4(nums2, 3)) // 0
+            }
+        }
+    }
+    /*
      A wiggle sequence is a sequence where the differences between successive numbers strictly alternate between positive and negative. The first difference (if one exists) may be either positive or negative. A sequence with one element and a sequence with two non-equal elements are trivially wiggle sequences.
      For example, [1, 7, 4, 9, 2, 5] is a wiggle sequence because the differences (6, -3, 5, -7, 3) alternate between positive and negative.
      In contrast, [1, 4, 7, 2, 5] and [1, 7, 4, 5, 5] are not wiggle sequences. The first is not because its first two differences are positive, and the second is not because its last difference is zero.
