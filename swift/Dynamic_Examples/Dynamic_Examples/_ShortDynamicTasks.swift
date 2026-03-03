c4d9d1d1001cfe67f4dcd17879521ec58a0d7984
Simon Getrik <SimonGetTrick@users.noreply.github.com>
2026-03-03 21:39:06 +0100

    388. Longest Absolute File Path

--- swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
+++ swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
@@ -27,6 +27,116 @@ extension Array where Element == Int {
 
 class Solution {
     /*
+     388. Longest Absolute File Path
+     Suppose we have a file system that stores both files and directories. An example of one system is represented in the following picture:
+     Here, we have dir as the only directory in the root. dir contains two subdirectories, subdir1 and subdir2. subdir1 contains a file file1.ext and subdirectory subsubdir1. subdir2 contains a subdirectory subsubdir2, which contains a file file2.ext.
+     In text form, it looks like this (with ⟶ representing the tab character):
+
+     dir
+     ⟶ subdir1
+     ⟶ ⟶ file1.ext
+     ⟶ ⟶ subsubdir1
+     ⟶ subdir2
+     ⟶ ⟶ subsubdir2
+     ⟶ ⟶ ⟶ file2.ext
+     If we were to write this representation in code, it will look like this: "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext". Note that the '\n' and '\t' are the new-line and tab characters.
+
+     Every file and directory has a unique absolute path in the file system, which is the order of directories that must be opened to reach the file/directory itself, all concatenated by '/'s. Using the above example, the absolute path to file2.ext is "dir/subdir2/subsubdir2/file2.ext". Each directory name consists of letters, digits, and/or spaces. Each file name is of the form name.extension, where name and extension consist of letters, digits, and/or spaces.
+
+     Given a string input representing the file system in the explained format, return the length of the longest absolute path to a file in the abstracted file system. If there is no file in the system, return 0.
+
+     Note that the testcases are generated such that the file system is valid and no file or directory name has length 0.
+
+      
+
+     Example 1:
+
+
+     Input: input = "dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext"
+     Output: 20
+     Explanation: We have only one file, and the absolute path is "dir/subdir2/file.ext" of length 20.
+     Example 2:
+
+
+     Input: input = "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"
+     Output: 32
+     Explanation: We have two files:
+     "dir/subdir1/file1.ext" of length 21
+     "dir/subdir2/subsubdir2/file2.ext" of length 32.
+     We return 32 since it is the longest absolute path to a file.
+     Example 3:
+
+     Input: input = "a"
+     Output: 0
+     Explanation: We do not have any files, just a single directory named "a".
+      
+
+     Constraints:
+
+     1 <= input.length <= 104
+     input may contain lowercase or uppercase English letters, a new line character '\n', a tab character '\t', a dot '.', a space ' ', and digits.
+     All file and directory names have positive length.
+     */
+    // 388. Longest Absolute File Path
+    // Stack of path lengths by depth
+    // Time: O(n)
+    // Space: O(depth)
+
+    enum Leet388 {
+        
+        class LongestAbsoluteFilePath {
+            
+            static func lengthLongestPath(_ input: String) -> Int {
+                
+                var maxLength = 0
+                
+                // depth -> current total length up to this depth
+                var stack: [Int] = [0] // base length
+                
+                let lines = input.split(separator: "\n", omittingEmptySubsequences: false)
+                
+                for line in lines {
+                    
+                    var name = String(line)
+                    var depth = 0
+                    
+                    while name.hasPrefix("\t") {
+                        depth += 1
+                        name.removeFirst()
+                    }
+                    
+                    // Ensure stack matches current depth
+                    while stack.count > depth + 1 {
+                        stack.removeLast()
+                    }
+                    
+                    let currentLength = stack.last! + name.count
+                    
+                    if name.contains(".") {
+                        // file
+                        maxLength = max(maxLength, currentLength)
+                    } else {
+                        // directory, add +1 for "/"
+                        stack.append(currentLength + 1)
+                    }
+                }
+                
+                return maxLength
+            }
+            
+            static func runDemo() {
+                let input1 = "dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext"
+                print(lengthLongestPath(input1)) // 20
+                
+                let input2 = "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"
+                print(lengthLongestPath(input2)) // 32
+                
+                let input3 = "a"
+                print(lengthLongestPath(input3)) // 0
+            }
+        }
+    }
+    /*
      386. Lexicographical Numbers
      Given an integer n, return all the numbers in the range [1, n] sorted in lexicographical order.
 
