fec458a9e255c2049c34293b6fce1eef5a7ea9c0
Simon Getrik <SimonGetTrick@users.noreply.github.com>
2026-03-08 10:15:42 +0100

    393. UTF-8 Validation

--- swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
+++ swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
@@ -27,6 +27,99 @@ extension Array where Element == Int {
 
 class Solution {
     /*
+     393. UTF-8 Validation
+     Given an integer array data representing the data, return whether it is a valid UTF-8 encoding (i.e. it translates to a sequence of valid UTF-8 encoded characters).
+
+     A character in UTF8 can be from 1 to 4 bytes long, subjected to the following rules:
+
+     For a 1-byte character, the first bit is a 0, followed by its Unicode code.
+     For an n-bytes character, the first n bits are all one's, the n + 1 bit is 0, followed by n - 1 bytes with the most significant 2 bits being 10.
+     This is how the UTF-8 encoding would work:
+
+          Number of Bytes   |        UTF-8 Octet Sequence
+                            |              (binary)
+        --------------------+-----------------------------------------
+                 1          |   0xxxxxxx
+                 2          |   110xxxxx 10xxxxxx
+                 3          |   1110xxxx 10xxxxxx 10xxxxxx
+                 4          |   11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+     x denotes a bit in the binary form of a byte that may be either 0 or 1.
+
+     Note: The input is an array of integers. Only the least significant 8 bits of each integer is used to store the data. This means each integer represents only 1 byte of data.
+
+      
+
+     Example 1:
+
+     Input: data = [197,130,1]
+     Output: true
+     Explanation: data represents the octet sequence: 11000101 10000010 00000001.
+     It is a valid utf-8 encoding for a 2-bytes character followed by a 1-byte character.
+     Example 2:
+
+     Input: data = [235,140,4]
+     Output: false
+     Explanation: data represented the octet sequence: 11101011 10001100 00000100.
+     The first 3 bits are all one's and the 4th bit is 0 means it is a 3-bytes character.
+     The next byte is a continuation byte which starts with 10 and that's correct.
+     But the second continuation byte does not start with 10, so it is invalid.
+     */
+    // 393. UTF-8 Validation
+    // Bit Manipulation
+    // Time: O(n)
+
+    enum Leet393 {
+        
+        class UTF8Validation {
+            
+            static func validUtf8(_ data: [Int]) -> Bool {
+                
+                var remaining = 0
+                
+                for num in data {
+                    
+                    let byte = num & 0xFF
+                    
+                    if remaining == 0 {
+                        
+                        if (byte >> 7) == 0 {
+                            continue
+                        }
+                        else if (byte >> 5) == 0b110 {
+                            remaining = 1
+                        }
+                        else if (byte >> 4) == 0b1110 {
+                            remaining = 2
+                        }
+                        else if (byte >> 3) == 0b11110 {
+                            remaining = 3
+                        }
+                        else {
+                            return false
+                        }
+                        
+                    } else {
+                        
+                        if (byte >> 6) != 0b10 {
+                            return false
+                        }
+                        
+                        remaining -= 1
+                    }
+                }
+                
+                return remaining == 0
+            }
+            
+            static func runDemo() {
+                
+                print(validUtf8([197,130,1])) // true
+                print(validUtf8([235,140,4])) // false
+                
+            }
+        }
+    }
+    /*
      392. Is Subsequence
      Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
      A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
