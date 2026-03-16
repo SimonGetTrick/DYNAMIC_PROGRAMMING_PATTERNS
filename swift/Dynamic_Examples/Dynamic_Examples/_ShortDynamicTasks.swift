fd9ddb52bd49316a9e9c78384eae093d6c5208ca
Simon Getrik <SimonGetTrick@users.noreply.github.com>
2026-03-16 19:40:17 +0100

    401. Binary Watch

--- swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
+++ swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
@@ -26,6 +26,71 @@ extension Array where Element == Int {
 }
 
 class Solution {
+    /*
+     401. Binary Watch
+     A binary watch has 4 LEDs on the top to represent the hours (0-11), and 6 LEDs on the bottom to represent the minutes (0-59). Each LED represents a zero or one, with the least significant bit on the right.
+
+     For example, the below binary watch reads "4:51".
+     Given an integer turnedOn which represents the number of LEDs that are currently on (ignoring the PM), return all possible times the watch could represent. You may return the answer in any order.
+     The hour must not contain a leading zero.
+     For example, "01:00" is not valid. It should be "1:00".
+     The minute must consist of two digits and may contain a leading zero.
+     For example, "10:2" is not valid. It should be "10:02".
+     Example 1:
+     Input: turnedOn = 1
+     Output: ["0:01","0:02","0:04","0:08","0:16","0:32","1:00","2:00","4:00","8:00"]
+     Example 2:
+     Input: turnedOn = 9
+     Output: []
+     Constraints:
+     0 <= turnedOn <= 10
+     */
+    // 401. Binary Watch
+    // Brute Force
+    // Time O(720)
+
+    enum Leet401 {
+        
+        class BinaryWatch {
+            
+            static func readBinaryWatch(_ turnedOn: Int) -> [String] {
+                
+                var result: [String] = []
+                
+                for h in 0..<12 {
+                    for m in 0..<60 {
+                        
+                        if bitCount(h) + bitCount(m) == turnedOn {
+                            
+                            let minute = m < 10 ? "0\(m)" : "\(m)"
+                            result.append("\(h):\(minute)")
+                        }
+                    }
+                }
+                
+                return result
+            }
+            
+            static func bitCount(_ x: Int) -> Int {
+                
+                var n = x
+                var count = 0
+                
+                while n > 0 {
+                    count += n & 1
+                    n >>= 1
+                }
+                
+                return count
+            }
+            
+            static func runDemo() {
+                
+                print(readBinaryWatch(1))
+                
+            }
+        }
+    }
     /*400. Nth Digit
      Given an integer n, return the nth digit of the infinite integer sequence [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ...].
      Example 1:
