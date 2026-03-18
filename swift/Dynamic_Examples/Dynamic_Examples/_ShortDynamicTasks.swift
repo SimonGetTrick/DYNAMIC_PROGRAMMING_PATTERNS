9bb914e710b74518723b5d4e5e3159eb36559113
Simon Getrik <SimonGetTrick@users.noreply.github.com>
2026-03-18 19:59:41 +0100

    406. Queue Reconstruction by Height

--- swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
+++ swift/Dynamic_Examples/Dynamic_Examples/_ShortDynamicTasks.swift
@@ -27,6 +27,61 @@ extension Array where Element == Int {
 
 class Solution {
     /*
+     406. Queue Reconstruction by Height
+     You are given an array of people, people, which are the attributes of some people in a queue (not necessarily in order). Each people[i] = [hi, ki] represents the ith person of height hi with exactly ki other people in front who have a height greater than or equal to hi.
+
+     Reconstruct and return the queue that is represented by the input array people. The returned queue should be formatted as an array queue, where queue[j] = [hj, kj] is the attributes of the jth person in the queue (queue[0] is the person at the front of the queue).
+     Example 1:
+     Input: people = [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
+     Output: [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]]
+     Explanation:
+     Person 0 has height 5 with no other people taller or the same height in front.
+     Person 1 has height 7 with no other people taller or the same height in front.
+     Person 2 has height 5 with two persons taller or the same height in front, which is person 0 and 1.
+     Person 3 has height 6 with one person taller or the same height in front, which is person 1.
+     Person 4 has height 4 with four people taller or the same height in front, which are people 0, 1, 2, and 3.
+     Person 5 has height 7 with one person taller or the same height in front, which is person 1.
+     Hence [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]] is the reconstructed queue.
+     Example 2:
+     Input: people = [[6,0],[5,0],[4,0],[3,2],[2,2],[1,4]]
+     Output: [[4,0],[5,0],[2,2],[3,2],[1,4],[6,0]]
+     */
+    // 406. Queue Reconstruction by Height
+    // Greedy + Sorting
+    // Time O(n^2)
+
+    enum Leet406 {
+        
+        class QueueReconstruction {
+            
+            static func reconstructQueue(_ people: [[Int]]) -> [[Int]] {
+                
+                // sort: height desc, k asc
+                let sorted = people.sorted {
+                    if $0[0] == $1[0] {
+                        return $0[1] < $1[1]
+                    }
+                    return $0[0] > $1[0]
+                }
+                
+                var result: [[Int]] = []
+                
+                for person in sorted {
+                    result.insert(person, at: person[1])
+                }
+                
+                return result
+            }
+            
+            static func runDemo() {
+                
+                let input = [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
+                print(reconstructQueue(input))
+                
+            }
+        }
+    }
+    /*
      403. Frog Jump Hard
      A frog is crossing a river. The river is divided into some number of units, and at each unit, there may or may not exist a stone. The frog can jump on a stone, but it must not jump into the water.
      Given a list of stones positions (in units) in sorted ascending order, determine if the frog can cross the river by landing on the last stone. Initially, the frog is on the first stone and assumes the first jump must be 1 unit.
